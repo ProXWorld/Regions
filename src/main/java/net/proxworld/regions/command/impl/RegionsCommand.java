84e98bb042ffb59199b327e0a16dbbb8d4fdd9b1
leywil <boss.leywil@bk.ru>
2024-09-19 03:33:38 +0300

    сделал таб комплит и новую команду

--- src/main/java/net/proxworld/regions/command/impl/RegionsCommand.java
+++ src/main/java/net/proxworld/regions/command/impl/RegionsCommand.java
@@ -1,7 +1,6 @@
 package net.proxworld.regions.command.impl;
 
 import com.sk89q.worldedit.math.BlockVector3;
-import com.sk89q.worldguard.protection.regions.ProtectedCuboidRegion;
 import lombok.AccessLevel;
 import lombok.NonNull;
 import lombok.experimental.FieldDefaults;
@@ -11,18 +10,12 @@ import me.darkakyloff.api.utils.PlayerUtils;
 import me.darkakyloff.api.utils.SoundUtils;
 import net.proxworld.regions.RegionPlugin;
 import net.proxworld.regions.menu.RegionMenu;
-import org.bukkit.Bukkit;
-import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-import java.util.List;
 
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
-public final class RegionsCommand extends APICommand implements TabCompleter {
+public final class RegionsCommand extends APICommand {
 
     RegionPlugin plugin;
 
@@ -78,20 +71,4 @@ public final class RegionsCommand extends APICommand implements TabCompleter {
         return true;
     }
 
-    @Override
-    public @Nullable List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
-        if (!PlayerUtils.instanceOfPlayer(commandSender)) return null;
-
-        if (strings.length == 1) {
-            return List.of("add", "remove");
-        }
-
-        if (strings.length > 1) {
-            return Bukkit.getOnlinePlayers().stream()
-                    .map(Player::getName)
-                    .toList();
-        }
-
-        return List.of();
-    }
 }
